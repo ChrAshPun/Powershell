4ca38b832fcdd819b8191421aef1c84f6a139769
ChrAshPun <punlac1222@gmail.com>
2019-11-15 18:40:02 -0800

    Update Get all mailbox sizes.ps1

--- Get all mailbox sizes.ps1
+++ Get all mailbox sizes.ps1
@@ -1,4 +1,3 @@
-
 # Get all mailbox sizes 
 
-Get-Mailbox –OrganizationalUnit <organizationalunit> -ResultSize Unlimited | Get-MailboxStatistics | select DisplayName,StorageLimitStatus, @{name="TotalItemSize (MB)"; expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1MB),2)}},ItemCount | Sort "TotalItemSize (MB)" -Descending | Export-csv –path "C:\Temp\MailboxSizes.csv" -NoTypeInformation 
+Get-Mailbox –OrganizationalUnit <organizational unit> -ResultSize Unlimited | Get-MailboxStatistics | select DisplayName,StorageLimitStatus, @{name="TotalItemSize (MB)"; expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1MB),2)}},ItemCount | Sort "TotalItemSize (MB)" -Descending | Export-csv –path "C:\Temp\MailboxSizes.csv" -NoTypeInformation 
